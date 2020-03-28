03f9d268942dbae00e7854e2b27b06ae55e04ffd
Nicolache <soundriver@mail.ru>
2020-03-28 13:29:15 +0300

    Provisioner connects now. 'sshKeys' was replace with 'ssh-keys'. There is a problem with docker.deploy.sh . It doesn't work wrom provisioner.

--- modules/docker/docker.tf
+++ modules/docker/docker.tf
@@ -14,17 +14,23 @@ resource "google_compute_instance" "docker" {
     }
   }
   metadata = {
-    sshKeys = "dockeruser:${file("${var.public_key_path}")}"
+    ssh-keys = "dockeruser:${file("${var.public_key_path}")}"
   }
   provisioner "remote-exec" {
     script = "files/docker.deploy.sh"
+    # inline = [
+    #   "sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D",
+    #   "sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'",
+    #   "sudo apt-get update",
+    #   "sudo apt-get install -y docker-engine",
+    # ]
   }
   connection {
     host = google_compute_address.docker_ip.address
     type = "ssh"
     user = "dockeruser"
     agent = false
-    private_key = file("${var.private_key_path}")
+    private_key = file(var.private_key_path)
   }
 }
 
