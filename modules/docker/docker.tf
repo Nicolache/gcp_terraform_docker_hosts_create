ecb694b0241e82fa21e69d58783dbd039af2fe02
Nicolache <soundriver@mail.ru>
2020-04-06 10:38:47 +0300

    sshguard is being disabled at the deploy since now.

--- modules/docker/docker.tf
+++ modules/docker/docker.tf
@@ -18,12 +18,6 @@ resource "google_compute_instance" "docker" {
   }
   provisioner "remote-exec" {
     script = "files/docker.deploy.sh"
-    # inline = [
-    #   "sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D",
-    #   "sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'",
-    #   "sudo apt-get update",
-    #   "sudo apt-get install -y docker-engine",
-    # ]
   }
   connection {
     host = google_compute_address.docker_ip.address
